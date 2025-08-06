64ae559eb9666b2640766dded04148b9976f08c9
marathon <QSfTfSilinaRoza@gmail.com>
2025-08-06 16:17:09 -0700

    Project create

---
+++ WebApplication1/Controllers/FirstController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class FirstController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World";
+        }
+    }
+}
