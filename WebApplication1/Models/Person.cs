d5bddc07ee96e064f08dfef479554f678a27a868
marathon <QSfTfSilinaRoza@gmail.com>
2025-08-10 08:49:57 -0700

    Remove old infra

--- WebApplication1/Models/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.Models
-{
-    public class Person
-    {
-        public string Name { get; set; } = String.Empty;
-        public int Age { get; set; }
-        public string Location { get; set; } = String.Empty;
-    }
-}
--- WebApplication1/Models/ProductResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using WebApplication1.Models;
-
-namespace WebApplication1.Models
-{
-    public class ProductResponse
-    {
-        public SingleProduct[] products { get; set; } = [];
-    }
-}
